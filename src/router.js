f67213903d7201e03c40d1a9914bf010ef084388
D3v3l0per1 <manuel.widmoser12@gmail.com>
2018-08-31 18:46:35 +0200

    Sync or so

--- src/router.js
+++ src/router.js
@@ -8,18 +8,11 @@ import Contact from './views/Contact'
 
 import UnderConstructions from './views/Shared/UnderConst'
 
-<<<<<<< HEAD
 /* eslint-disable */
 
 Vue.use(Router)
 const router = new Router({
-=======
-Vue.use(Router)
-
-export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
->>>>>>> aa06ae9741f732fdcce923f86cb7dac71ee059d6
   routes: [
     {
       path: '/',
@@ -53,7 +46,6 @@ export default new Router({
     }
   ]
 })
-<<<<<<< HEAD
 
 router.beforeResolve((to, from, next) => {
   if (to.name) {
@@ -67,5 +59,3 @@ router.afterEach((to, from) => {
 })
 
 export default router
-=======
->>>>>>> aa06ae9741f732fdcce923f86cb7dac71ee059d6
